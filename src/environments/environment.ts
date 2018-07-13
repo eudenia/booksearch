a9788bab73013a43972dd8bba2d83f98d189a02c
Eudenia <eudenia@gmail.com>
2018-07-13 11:14:02 -0300

    resetado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  googleURL:'https://www.googleapis.com/books/v1/volumes?projection=lite&key='
+  production: false
 };
 
 /*
